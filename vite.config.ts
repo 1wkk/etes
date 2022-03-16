5860b231516604a1b5cc04b277206ebed5c2e1e8
ryanmoyo <iherewithmyheart@gmail.com>
2022-03-17 01:28:38 +0800

    🔧 config - ts paths, include - vite alias

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@/reactivity': path.resolve(__dirname, '/packages/reactivity')
+    }
+  }
+})
